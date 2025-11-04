0b4c95a682fcbe4bdfaded524724bd8f4cf2b175
Sachin Singh Yadav <sachin.singh@gocomet.com>
2025-11-04 14:08:18 +0530

    Removed api end point

--- backend/src/routes/todo.routes.ts
+++ backend/src/routes/todo.routes.ts
@@ -3,13 +3,6 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.status(200).json({
-    first_name: 'Sachin Singh',
-    last_name: 'Yadav',
-  });
-});
-
 router.get('/get_todos', todoControllers.getTodos);
 
 router.post('/create-todo', todoControllers.createTodo);
